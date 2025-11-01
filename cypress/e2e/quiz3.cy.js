592e2389dc33b1f27c5f37d785ef84ddd27fa470
dafadwifahreza <dafadwifahreza@gmail.com>
2025-11-02 02:04:38 +0700

    first commit

---
+++ cypress/e2e/quiz3.cy.js
@@ -0,0 +1,59 @@
+describe('Scenario Verifikasi Fungsi Login',()=>{
+    //Pengguna login menggunakan data valid
+    it('TC01-Login dengan Username dan Password benar',()=>{ 
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('input[name="username"]').type('Admin').should('have.value','Admin')
+        cy.get('input[name="password"]').type('admin123')
+        cy.get('button[type="submit"]').click().should('be.visible')
+        cy.url().should('include','dashboard')
+    })
+
+    //Pengguna login menggunakan data invalid
+    it('TC02-Login dengan username benar dan password salah',()=>{
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('input[name="username"]').type('Admin').should('have.value','Admin')
+    cy.get('input[name="password"]').type('123')
+    cy.get('button[type="submit"]').click().should('be.visible')
+    cy.get('.oxd-alert').should('contain', 'Invalid credentials')
+  });
+  
+    //Pengguna login menggunakan data invalid
+    it('TC03-Login tanpa memasukan username',()=>{
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click().should('be.visible')
+    cy.get('.oxd-input-field-error-message').should('contain', 'Required')
+  });
+
+     //Pengguna login menggunakan data invalid
+    it('TC04-Login tanpa memasukan password',()=>{
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('input[name="username"]').type('Admin').should('have.value','Admin')
+    cy.get('button[type="submit"]').click().should('be.visible')
+    cy.get('.oxd-input-field-error-message').should('contain', 'Required')
+  });
+
+    //Pengguna login menggunakan data invalid
+    it('TC05-Login tanpa memasukan username dan password', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get('button[type="submit"]').click();
+    cy.get('.oxd-input-field-error-message').should('contain', 'Required')
+  });
+
+   //Pengguna login menggunakan data invalid
+    it('TC06-Login dengan Username dan Password salah',()=>{ 
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('input[name="username"]').type('admin123')
+        cy.get('input[name="password"]').type('123')
+        cy.get('button[type="submit"]').click().should('be.visible')
+        cy.get('.oxd-alert').should('contain','Invalid credentials')
+    })
+
+     //Pengguna akses halaman reset password
+    it('TC07-Pengguna akses halaman reset password',()=>{ 
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.contains('Forgot your password?').click();
+        cy.url().should('include','requestPasswordReset');
+        cy.get('.orangehrm-card-container').should('be.visible');
+    })
+})
