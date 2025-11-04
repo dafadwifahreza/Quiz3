a189ac495ba3f566b4a5bb4db1dc4c9e1ecf4dea
dafadwifahreza <dafadwifahreza@gmail.com>
2025-11-04 22:29:17 +0700

    update file tugas16.cy.js

--- cypress/e2e/tugas-harian/tugas16.cy.js
+++ cypress/e2e/tugas-harian/tugas16.cy.js
@@ -21,28 +21,5 @@ describe ('Scenario Verifikasi Fungsi Login', () => {
         cy.wait('@failedLogin').its('response.statusCode').should('eq', 302)
     })
 
-    //Pengguna akses halaman reset password
-    it('TC07-Pengguna akses halaman reset password',()=>{
-        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-        cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode').as  ('resetPass')
-        cy.contains('Forgot your password?').click();
-        cy.url().should('include','requestPasswordReset');
-        cy.get('.orangehrm-card-container').should('be.visible');
-        cy.wait('@resetPass').its('response.statusCode').should('eq', 200)
-    })
-
-
-
-
-//      //Pengguna login menggunakan data invalid
-//     it('TC03-Login tanpa memasukan username',()=>{
-//     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-//     cy.get('input[name="password"]').type('admin123')
-//     cy.intercept('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'). as('missingUsername')
-//     cy.get('button[type="submit"]').click().should('be.visible')
-//     cy.get('.oxd-input-field-error-message').should('contain', 'Required')
-//     cy.wait('@missingUsername').its('response.statusCode').should('eq', 200)
-//   });
-
     
-});
+})
